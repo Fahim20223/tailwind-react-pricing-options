a15d6d273c372280ccf4af5bbf3852257bf92f59
Fahim2023 <fahimshahrier2023@gmail.com>
2025-09-30 16:27:54 +0600

    added many charts to the file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import axios from "axios";
 import "./App.css";
 import DaisyNav from "./Components/DaisyNav/DaisyNav";
+import MarksChart from "./Components/MarksChart/MarksChart";
 import NavBar from "./Components/NavBar/NavBar";
 import ResultChart from "./Components/ResultChart/ResultChart";
+import { Suspense } from "react";
+const marksPromise = axios("marksData.json");
 
 function App() {
   return (
@@ -12,6 +16,9 @@ function App() {
       </header>
       <main></main>
       <ResultChart></ResultChart>
+      <Suspense fallback={"Loading...."}>
+        <MarksChart marksPromise={marksPromise}></MarksChart>
+      </Suspense>
     </>
   );
 }
---
+++ src/Components/MarksChart/MarksChart.jsx
@@ -0,0 +1,42 @@
+import React, { use } from "react";
+import { Bar, BarChart, Legend, Tooltip, XAxis, YAxis } from "recharts";
+
+const MarksChart = ({ marksPromise }) => {
+  const marksRes = use(marksPromise);
+
+  const marksData = marksRes.data;
+  //   console.log(marksData);
+
+  //data processing to the chart
+
+  const marksDataChart = marksData.map((studentData) => {
+    const student = {
+      id: studentData.id,
+      name: studentData.name,
+      physics: studentData.marks.physics,
+      chemistry: studentData.marks.chemistry,
+      math: studentData.marks.math,
+      biology: studentData.marks.biology,
+    };
+    const average =
+      (student.physics + student.chemistry + student.biology + student.math) /
+      3;
+    student.average = average;
+    return student;
+  });
+  console.log(marksDataChart);
+  return (
+    <div>
+      <BarChart width={500} height={300} data={marksDataChart}>
+        <XAxis dataKey={"name"}></XAxis>
+        <YAxis></YAxis>
+        <Bar dataKey={"average"} fill="green"></Bar>
+        <Bar dataKey={"chemistry"} fill="blue"></Bar>
+        <Tooltip></Tooltip>
+        <Legend />
+      </BarChart>
+    </div>
+  );
+};
+
+export default MarksChart;
